2c712e49eeb08fcd7d0010f6f1a099821e6c48aa
Teako Warfield-Graham <teakowg@gmail.com>
2017-01-25 15:01:15 +0000

    First refactor of update_quality method into SRP methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -3,12 +3,85 @@ require_relative 'item.rb'
 
 class GildedRose
 
+  attr_reader :items
+
   def initialize(items)
     @items = items
   end
 
+  # def determine_item
+  #   items.each do |item|
+  #     if item.name == "Aged Brie"
+  #       aged_brie
+  #     else
+  #       "not brie"
+  #     end
+  #   end
+  # end
+  #
+  # def aged_brie
+  #   puts "I'm aged brie"
+  # end
+
+  def regular_item_quality
+    items.each do |item|
+      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+        if item.quality > 0
+          if item.name != "Sulfuras, Hand of Ragnaros"
+            item.quality = item.quality - 1
+          end
+        end
+      end
+    end
+  end
+
+  def backstage_pass_quality #needs to interact with the method that reduces sell_in as well...
+    items.each do |item|
+    if item.quality < 50
+      item.quality = item.quality + 1
+      if item.name == "Backstage passes to a TAFKAL80ETC concert"
+        if item.sell_in < 11
+          if item.quality < 50
+            item.quality = item.quality + 1
+          end
+        end
+        if item.sell_in < 6
+          if item.quality < 50
+            item.quality = item.quality + 1
+          end
+        end
+      end
+    end
+  end
+  end
+
+  def decrease_sell_by_date
+    if item.name != "Sulfuras, Hand of Ragnaros"
+      item.sell_in = item.sell_in - 1
+    end
+  end
+
+  def past_sell_by_date
+    if item.sell_in < 0
+      if item.name != "Aged Brie"
+        if item.name != "Backstage passes to a TAFKAL80ETC concert"
+          if item.quality > 0
+            if item.name != "Sulfuras, Hand of Ragnaros"
+              item.quality = item.quality - 1
+            end
+          end
+        else
+          item.quality = item.quality - item.quality
+        end
+      else
+        if item.quality < 50
+          item.quality = item.quality + 1
+        end
+  end
+
+
   def update_quality()
-    @items.each do |item|
+    items.each do |item|
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
