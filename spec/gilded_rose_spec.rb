2d82b345d902cffaf18204004cac8fad6d336f8d
Teako Warfield-Graham <teakowg@gmail.com>
2017-01-25 12:17:44 +0000

    Set up shell tests for current legacy code specs

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -10,8 +10,50 @@ describe GildedRose do
       expect(items[0].name).to eq "banana"
     end
 
+    it "lowers the quality value for each item at end of day" do
+      ###
+    end
+
+    it "lowers the sell by date for each item at end of day " do
+      ###
+    end
+
+    it "decreases item's quality twice as fast after sell by date" do
+      ###
+    end
+
+    it "doesn't allow a negative quality value " do
+      ###
+    end
 
 
+    it "doesn't allow a quality value to be greater than 50" do
+      ###
+    end
+
+    it "doesn't decrease sell by or quality values for Sulfuras" do
+      ###
+    end
+
+    it "increases quality of Aged Brie as it ages" do
+      ###
+    end
+
+    scenerio "Backstage passes increase in quality with age" do
+
+      it "increases quality value by 2 when sell by date < 10" do
+
+      end
+
+      it "increases quality value by 3 when sell by date < 5" do
+
+      end
+
+      it "sets quality value to 0 when sell by date is == 0" do
+
+      end
+
+    end
 
   end
 
