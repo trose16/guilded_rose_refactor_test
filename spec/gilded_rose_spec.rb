f065b05b2afe88e850d8e70e1801bc825859b7d1
Teako Warfield-Graham <teakowg@gmail.com>
2017-01-25 11:51:49 +0000

    Corrected first legacy test for item name

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,11 +3,11 @@ require 'gilded_rose'
 describe GildedRose do
 
   describe "#update_quality" do
-    
+
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
+      items = [Item.new("banana", 0, 0)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+      expect(items[0].name).to eq "banana"
     end
 
 
