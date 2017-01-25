82933f98812ccd4bbf04bea8edce700277eb8e4c
Teako Warfield-Graham <teakowg@gmail.com>
2017-01-25 14:03:42 +0000

    Tests for quality scores that raise errors

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -25,28 +25,38 @@ describe GildedRose do
       end
 
       it "decreases item's quality twice as fast after sell by date" do
-        ###
+        milk = [Item.new("milk", 0, 35)]
+        GildedRose.new(milk).update_quality
+        expect(milk[0].quality).to eq 33
       end
 
-      it "doesn't allow a negative quality value " do
-        ###
+    end
+
+    context "should throw an error when" do
+
+      it "has a negative quality value " do
+        milk = [Item.new("milk", 7, -5)]
+        GildedRose.new(milk).update_quality
+        expect { milk[0].quality }.to raise_error "Quality value is a negative number!"
       end
 
 
-      it "doesn't allow a quality value to be greater than 50" do
+      it "has quality value to be greater than 50" do
+        milk = [Item.new("milk", 7, 55)]
+        GildedRose.new(milk).update_quality
+        expect { milk[0].quality }.to raise_error "Quality value is above 50!"
+      end
+
+    end
+
+
+    context "special items don't degrade with age" do
+
+      it "increases quality of Aged Brie as it ages" do
         ###
       end
 
       it "doesn't decrease sell by or quality values for Sulfuras" do
-        ###
-      end
-      
-    end
-
-
-    context "special items increase in quality with age" do
-
-      it "increases quality of Aged Brie as it ages" do
         ###
       end
 
